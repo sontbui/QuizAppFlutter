2dfe0fca8f5c06e04353cf5dea2c82bb1dab947b
Bui Thanh Son <sonbuithanh306@gmail.com>
2023-12-19 05:13:56 +0700

    First commit

---
+++ lib/ListQuestion/question_list.dart
@@ -0,0 +1,204 @@
+import 'package:quizz_app/models/quiz_question.dart';
+
+const List<QuizQuestion> questions = [
+  // question1
+  QuizQuestion(
+    'In OOP, what does the term "encapsulation" refer to?',
+    [
+      'Creating objects from classes',
+      'Protecting the data and methods of an object',
+      'Inheriting features from another class.',
+      'Overloading operators',
+    ],
+  ),
+  // question2
+  QuizQuestion(
+    'What is the purpose of "inheritance" in OOP?',
+    [
+      'Creating objects from classes',
+      'Protecting the data and methods of an object',
+      'Inheriting features from another class.',
+      'Overloading operators',
+    ],
+  ),
+  // question3
+  QuizQuestion(
+    'What does the term "encapsulation" refer to in OOP?',
+    [
+      'Data hiding',
+      'Code duplication',
+      'Creating objects',
+      'Inheriting properties',
+    ],
+  ),
+  // question4
+  QuizQuestion(
+    'Polymorphism in OOP is associated with:',
+    [
+      'Multiple data types',
+      'Multiple forms',
+      'Multiple algorithms',
+      'Multiple classes',
+    ],
+  ),
+  // question5
+  QuizQuestion(
+    'What is a constructor in OOP used for?',
+    [
+      'Initializing objects',
+      'Creating classes',
+      'Defining methods',
+      'Handling exceptions',
+    ],
+  ),
+  // question6
+  QuizQuestion(
+    'Which OOP principle promotes code reuse?',
+    [
+      'Polymorphism',
+      'Encapsulation',
+      'Inheritance',
+      'Abstractions',
+    ],
+  ),
+  // question7
+  QuizQuestion(
+    'What is the purpose of an abstract class in OOP?',
+    [
+      'To create objects directly',
+      'To provide a blueprint for other classes',
+      'To hide data',
+      'To implement interfaces',
+    ],
+  ),
+  // question8
+  QuizQuestion(
+    'Which OOP concept allows a class to have multiple methods with the same name but different parameters?',
+    [
+      'Inheritance',
+      'Encapsulation',
+      'Polymorphism',
+      'Abstraction',
+    ],
+  ),
+  // question9
+  QuizQuestion(
+    'What is the purpose of the "super" keyword in OOP?',
+    [
+      'To call the parent class constructor',
+      'To create a new object',
+      'To access private members of a class',
+      'To implement interfaces',
+    ],
+  ),
+  // question10
+  QuizQuestion(
+    'What is the role of an interface in OOP?',
+    [
+      'To define a blueprint for a class',
+      'To hide implementation details',
+      'To provide a default implementation',
+      'To create objects directly',
+    ],
+  ),
+  // question11
+  QuizQuestion(
+    'What does the term "abstraction" mean in OOP?',
+    [
+      'Hiding implementation details and showing only essential features',
+      'Creating objects directly',
+      'Inheriting properties from another class',
+      'Overloading operators',
+    ],
+  ),
+  // question12
+  QuizQuestion(
+    'Why is polymorphism important in OOP?',
+    [
+      'To create objects directly',
+      'To define a blueprint for a class',
+      'To hide implementation details',
+      'To allow objects to be treated as instances of their parent class',
+    ],
+  ),
+  // question13
+  QuizQuestion(
+    'What is the purpose of the "this" keyword in OOP?',
+    [
+      'To call the parent class constructor',
+      'To create a new object',
+      'To access private members of a class',
+      'To refer to the current instance of the class',
+    ],
+  ),
+  // question14
+  QuizQuestion(
+    'What is a benefit of using encapsulation in OOP?',
+    [
+      'Increased code complexity',
+      'Improved code readability',
+      'Reduced code reusability',
+      'Elimination of object instances',
+    ],
+  ),
+  // question15
+  QuizQuestion(
+    'In OOP, what is a "singleton" class?',
+    [
+      'A class with multiple instances',
+      'A class that cannot be instantiated',
+      'A class with only one instance',
+      'A class without methods',
+    ],
+  ),
+  // question16
+  QuizQuestion(
+    'What is the purpose of the "static" keyword in OOP?',
+    [
+      'To create objects directly',
+      'To declare constant values',
+      'To define a blueprint for a class',
+      'To associate a variable or method with the class rather than instances of the class',
+    ],
+  ),
+  // question17
+  QuizQuestion(
+    'What is the primary goal of OOP principles?',
+    [
+      'Minimizing code readability',
+      'Maximizing code duplication',
+      'Improving code maintainability and reusability',
+      'Eliminating polymorphism',
+    ],
+  ),
+  // question18
+  QuizQuestion(
+    'What is the term for the ability of a class to have multiple methods with the same name but different parameters?',
+    [
+      'Encapsulation',
+      'Polymorphism',
+      'Inheritance',
+      'Abstraction',
+    ],
+  ),
+  // question19
+  QuizQuestion(
+    'What is the purpose of the "new" keyword in OOP?',
+    [
+      'To declare a variable',
+      'To create a new class',
+      'To instantiate an object',
+      'To access a static method',
+    ],
+  ),
+  // question20
+  QuizQuestion(
+    'What is the term for the process of creating a new class based on an existing class?',
+    [
+      'Inheritance',
+      'Polymorphism',
+      'Abstraction',
+      'Composition',
+    ],
+  ),
+];
